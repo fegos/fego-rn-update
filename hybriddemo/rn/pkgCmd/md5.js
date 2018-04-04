9f1ad8ae967c852efc93f54791bc4aa4fc7eccb6
史晓霞 <dx910114@126.com>
2018-04-04 16:38:10 +0800

    MOD：优化

--- hybriddemo/rn/pkgCmd/md5.js
+++ hybriddemo/rn/pkgCmd/md5.js
@@ -1,20 +1,10 @@
 var fs = require('fs');
 var crypto = require('crypto');
-/**
- * 生成文件md5值
- * @param {*} filepath 
- */
-function generateFileMd5(filepath) {
-	var buffer = fs.readFileSync(filepath);
-	var fsHash = crypto.createHash('md5');
-	fsHash.update(buffer);
-	var md5 = fsHash.digest('hex');
-	return md5;
-}
+let Utils = require('./Utils');
 
 filepath = process.argv[2];
 apkVer = process.argv[3];
 zipName = process.argv[4];
 content = process.argv[5];
 
-fs.writeFileSync(filepath + 'config', content + '_' + generateFileMd5(filepath + apkVer + '/' + zipName));
+fs.writeFileSync(filepath + 'config', content + '_' + Utils.generateFileMd5(filepath + apkVer + '/' + zipName));
