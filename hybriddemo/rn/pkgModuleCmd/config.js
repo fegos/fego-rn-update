5ba1830e85d939fa5b9c6c9fffe6ab685b370320
史晓霞 <dx910114@126.com>
2018-04-04 16:23:58 +0800

    MOD：test 非module

--- hybriddemo/rn/pkgModuleCmd/config.js
+++ hybriddemo/rn/pkgModuleCmd/config.js
@@ -5,7 +5,7 @@ let map = {
 	 * 1、username为电脑用户名；
 	 * 2、path为包存储位置，末尾需要加“/”，否则会报路径错误
 	 */
-	sxiaoxia: '/Users/sxiaoxia/Desktop/work/kaiyuan/fego-rn-update/hybriddemo/rn/bao/',
+	sxiaoxia: '/Users/sxiaoxia/Desktop/work/kaiyuan/fego-rn-update/hybriddemo/rn/testbao/',
 }
 // 获取系统信息
 let os = require('os');
