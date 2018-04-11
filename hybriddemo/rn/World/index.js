897627f888d2ed4faf339f6594a76d1ea374f52b
史晓霞 <dx910114@126.com>
2018-04-11 16:30:13 +0800

    MOD：临时测试

--- hybriddemo/rn/World/index.js
+++ hybriddemo/rn/World/index.js
@@ -13,8 +13,9 @@ export default class World extends Component {
 					onPress={() => {
 						FegoRNUpdate.hotReload("World");
 					}}>
-					<Text style={styles.btnText}>热更新测试</Text>
+					<Text>热更新测试</Text>
 				</TouchableHighlight>
+				<Text>我要更新了</Text>
 			</View>
 		)
 	}
