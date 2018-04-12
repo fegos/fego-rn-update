ed9328456e5b521f5616e6c57dd34098836f56f7
史晓霞 <dx910114@126.com>
2018-04-12 11:03:11 +0800

    MOD：使用拆包脚本test

--- demo/App.js
+++ demo/App.js
@@ -24,11 +24,11 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+	// <Image source={require('./img/app.png')} style={{ width: 50, height: 59 }} />
+	// <Text style={[styles.welcome, { fontFamily: 'song' }]}>&#xe61f;</Text>
 	render() {
 		return (
 			<View style={styles.container}>
-				<Image source={require('./img/app.png')} style={{ width: 50, height: 59 }} />
-				<Text style={[styles.welcome, { fontFamily: 'song' }]}>&#xe61f;</Text>
 				<Text style={styles.welcome}>Welcome to FegoRNUpdate!</Text>
 				<Text style={styles.instructions}>To get started, edit App.js</Text>
 				<Text style={styles.instructions}>
@@ -37,7 +37,7 @@ export default class App extends Component {
 				<TouchableHighlight
 					underlayColor="transparent"
 					onPress={() => {
-						FegoRNUpdate.hotReload();
+						FegoRNUpdate.hotReload("");
 					}}>
 					<Text style={styles.btnText}>热更新测试</Text>
 				</TouchableHighlight>
